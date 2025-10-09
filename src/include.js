0babc2fdd4f7447ffa893fcaaa9ad55b37a7a608
Annika von Bayern <annikawespe@gmail.com>
2025-10-09 09:22:35 +0200

    basic navigation

--- src/include.js
+++ src/include.js
@@ -1,10 +1,62 @@
-// Mobile menu toggle
-const menuBtn = document.getElementById('menu-btn');
-const mobileMenu = document.getElementById('mobile-menu');
-
-menuBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+document.addEventListener("DOMContentLoaded", function () {
+  loadContent();
+  loadEmailAddress();
+  loadHeaderAndFooterFunctions();
 });
 
-// Year in footer
-document.getElementById('year').textContent = new Date().getFullYear();
+cleanPath();
+
+function cleanPath() {
+  const allowedPaths = [
+    "/",
+    "/essays",
+    "/essays.html",
+    "/core",
+    "/core.html",
+    "/speaking",
+    "/speaking.html",
+    "/collaborate",
+    "/collaborate.html",
+    "/about",
+    "/about.html",
+  ];
+  const currentPath = window.location.pathname;
+  if (!allowedPaths.includes(currentPath)) {
+    window.history.replaceState({}, "", "/");
+  }
+}
+
+function loadHeaderAndFooterFunctions() {
+  // Mobile menu toggle
+  const menuBtn = document.getElementById("menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  // Year in footer
+  document.getElementById("year").textContent = new Date().getFullYear();
+}
+
+function loadContent() {
+  fetch("pages/essays.html")
+    .then((res) => res.text())
+    .then((data) => {
+      const element = document.getElementById("essays");
+      if (element) {
+        element.innerHTML = data;
+      }
+    });
+}
+
+function loadEmailAddress() {
+  const user = "annika";
+  const domain = "foldingai.org";
+  const mail = `${user}@${domain}`;
+  const linktext = `${mail}`;
+
+  document.querySelectorAll(".mailadresse").forEach((el) => {
+    el.innerHTML = `<a href="mailto:${mail}">${linktext}</a>`;
+  });
+}
